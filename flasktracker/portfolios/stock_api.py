10420ca03e5ba44a39769b83b5ce2eb00df6ee70
Ichiro Wang <ichiwang27@gmail.com>
2024-12-29 20:24:30 -0800

    stock api tweaks

--- flasktracker/portfolios/stock_api.py
+++ flasktracker/portfolios/stock_api.py
@@ -1,6 +1,32 @@
-def get_price(ticker: str) -> dict:
-    test = {"market": 13, "change": 1.00}
-    test_values = {"VFV": {"market": 150, "change": 1.04}, "RY": {"market": 175, "change": 0.39},
-                   "SHOP": {"market": 110, "change": -1.64}, "TD": {"market": 80, "change": 0.26},
-                   "TEST": test}
-    return test_values.get(ticker, test)
+import os
+import requests
+
+
+def get_data(ticker: str):
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function": "GLOBAL_QUOTE",
+        "symbol": ticker,
+        "apikey": os.getenv('STOCK_API')
+    }
+    # print(requests.get(url, params=params))
+    return requests.get(url, params=params)
+
+
+def get_price(ticker: str):
+    ticker = ticker.upper()
+    if ticker == "TEST.TO":
+        return {"market": 100, "change": 1.00}
+    if ".TO" in ticker:
+        ticker = ticker.replace(".TO", ".TRT")
+
+    response = get_data(ticker)
+    if not response.ok:
+        return None
+    data = response.json()
+    global_quote = data.get("Global Quote", None)
+    if not global_quote:
+        return None
+    price = float(data["Global Quote"]["05. price"])
+    change_percent = float(data["Global Quote"]["10. change percent"].strip("%"))
+    return {"market": price, "change": change_percent}
